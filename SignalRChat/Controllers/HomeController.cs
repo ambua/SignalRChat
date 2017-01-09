0acf8044705056148ee56da1bc5a98dc8f7c11b1
Alltag <Alltag@WORKSTATIONPC>
2017-01-09 21:27:13 +0100

    SignalR impl

--- SignalRChat/Controllers/HomeController.cs
+++ SignalRChat/Controllers/HomeController.cs
@@ -8,6 +8,11 @@ namespace SignalRChat.Controllers
 {
     public class HomeController : Controller
     {
+        public ActionResult Chat()
+        {
+            return View();
+        }
+
         public ActionResult Index()
         {
             return View();
